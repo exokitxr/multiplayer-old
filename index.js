f30280cc312bd2cbf4c07bc77ced9f9f3557039f
Avaer Kazmer <a@modules.io>
2018-05-16 21:33:22 -0400

    Clean up servers list rendering

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ app.get('/', (req, res, next) => {
 <head></head>
 <body>
   <h1>Mutiplayer servers (live)</h1>
-  ${servers.map(server => `<a href="/${server.name}">${'⌨️\xa0' + server.name}</a><br>`).join('\n')}
+  ${servers.map(server => `<a href="/servers/${server.name}">${'⌨️\xa0/servers/' + server.name}</a><br>`).join('\n')}
 </body>
 </html>
 `);
